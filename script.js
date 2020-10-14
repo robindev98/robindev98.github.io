d7676d69007d77160a8f52439f811e2a0f799289
robin <robindavid10@hotmail.fr>
2020-10-14 09:31:42 +0200

    commencement personalisation produit

--- script.js
+++ script.js
@@ -4,7 +4,20 @@
 		alert('ok');
 		
 	}
+	/* Lorqsue l'on clique sur un onglet parmis la liste */
 
+	var tabs = document.querySelectorAll('.tabs a')
+	for (var i = 0; i < tabs.lenght; i++) {
+		tabs[i].addEventListener('click', function (e) {
+
+			if (rhis.parentNode.classList.contains('active')) {
+				return false
+			}
+
+			var div = this.parentNode..parentNode.parentNode
+			div.querySelector('.tabs .active').classList.remove('active')
+		})
+	}
 	
 
 </script>
